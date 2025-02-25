a4560db1aa1733a0a38df7abeeb2c388fa4b64bf
0098-sayan <maitysayan704@gmail.com>
2025-02-25 12:16:37 +0530

    Currency converter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='bg-green-500 text-2xl font-bold justify-center items-center flex'>Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
